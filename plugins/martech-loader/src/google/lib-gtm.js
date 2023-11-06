8683f4a1a6ce19c744d445c0329e642dce6e21e7
Francisco Chicharro Sanz <chicharr@adobe.com>
2023-11-06 15:53:10 +0100

    Fix gtm with partytown

--- plugins/martech-loader/src/google/lib-gtm.js
+++ plugins/martech-loader/src/google/lib-gtm.js
@@ -25,15 +25,18 @@ export default function loadGTMScript(config) {
       j.src =
           'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
       f.parentNode.insertBefore(j, f);
-  })(window, document, 'script', 'dataLayer', '${gtmId}');
-  window.dataLayer = window.dataLayer || [];
+  })(window, document, 'script', 'dataLayer', '${gtmId}');`
+
+  const scriptBodyTag = document.createElement('script');
+  scriptBodyTag.innerHTML = `window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
-  gtag('set', {
-      'cookie_flags': 'SameSite=None;Secure'
-  });
-  `;
+  window.gtag = gtag;`;
+
   if (webworker && webworker.toLowerCase() === 'yes') {
     scriptTag.type = 'text/partytown';
+    scriptBodyTag.type = 'text/partytown';
   }
+
   document.head.prepend(scriptTag);
+  document.body.prepend(scriptBodyTag);
 }
